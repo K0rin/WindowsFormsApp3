85e9d606f3377c5d9d01f25eb4b85dfab68e1290
K0rin <34002181+K0rin@users.noreply.github.com>
2023-09-27 16:21:58 +0300

    Add project files.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(54, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(348, 251);
+            this.label1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(235, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(71, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tickets Sold";
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(429, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(348, 251);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(99, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 26);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Class A:";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(99, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 26);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Class B:";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(99, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 26);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Class C:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(235, 121);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(235, 165);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(448, 78);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(104, 26);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Class A:";
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(448, 121);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(104, 26);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Class B:";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(448, 165);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(104, 26);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Class C:";
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(558, 78);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(203, 26);
+            this.label10.TabIndex = 12;
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(558, 121);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(203, 26);
+            this.label11.TabIndex = 13;
+            // 
+            // label12
+            // 
+            this.label12.Location = new System.Drawing.Point(558, 165);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(203, 26);
+            this.label12.TabIndex = 14;
+            // 
+            // label13
+            // 
+            this.label13.Location = new System.Drawing.Point(452, 50);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(161, 23);
+            this.label13.TabIndex = 15;
+            this.label13.Text = "Revenue Generated";
+            // 
+            // label14
+            // 
+            this.label14.Location = new System.Drawing.Point(558, 210);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(203, 26);
+            this.label14.TabIndex = 16;
+            // 
+            // label15
+            // 
+            this.label15.Location = new System.Drawing.Point(448, 210);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(104, 26);
+            this.label15.TabIndex = 17;
+            this.label15.Text = "Total:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(127, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 52);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Calculate Revenue";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(314, 364);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 52);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(480, 364);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 52);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double classA = 0;
+            double classB = 0;
+            double classC = 0;
+            string errorList="";
+            try 
+            {
+                classA = double.Parse(textBox1.Text)*15;
+                label10.Text = classA.ToString()+" $";
+            } 
+            catch 
+            {
+                if (textBox1.Text.Equals(""))
+                {
+                    errorList = "Class A has no value \n";
+                }
+                else 
+                {
+                    errorList = "Class A has wrong value \n";
+                }               
+            }
+            try
+            {
+                classB = double.Parse(textBox2.Text) * 12;
+                label11.Text = classB.ToString() + " $";
+            }
+            catch
+            {
+                if (textBox2.Text.Equals("")) 
+                {
+                    errorList = errorList + "Class B has no value \n";
+                }
+                else 
+                {
+                    errorList = errorList + "Class B has wrong value \n";
+                }                   
+            }
+            try
+            {
+                classC = double.Parse(textBox3.Text) * 9;
+                label12.Text = classC.ToString() + " $";
+            }
+            catch
+            {
+                if (textBox3.Text.Equals(""))
+                {
+                    errorList = errorList + "Class C has no value \n";
+                }
+                else
+                {
+                    errorList = errorList + "Class C has wrong value \n";
+                }
+            }
+            if (errorList.Equals(""))
+            {
+                double summ = classA + classB + classC;
+                label14.Text = summ.ToString()+" $";
+            }
+            else 
+            {
+                MessageBox.Show(errorList);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label10.Text = "";
+            label11.Text = "";
+            label12.Text = "";
+            label14.Text = "";
+        }
+    }
+}
